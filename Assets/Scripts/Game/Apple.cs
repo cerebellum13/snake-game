373fe2fffb64688fb3ac781723dadffac4468c8d
Nikita Maslovski <nikita.maslovski13@gmail.com>
2022-03-15 17:44:25 +0300

    Implement basic tale rotation

--- Assets/Scripts/Game/Apple.cs
+++ Assets/Scripts/Game/Apple.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    float minPosition = -23.5f;
+    float maxPosition = 23.5f;
+    float stepSize = 0.5f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        ScoreSystem.increaseScore();
+        // Snake.GetInstance().AddSnakeTale();
+        gameObject.transform.position = generateNewPosition();
+    }
+
+    private Vector3 generateNewPosition()
+    {
+        return new Vector3(GetRandomPositionPoint(), GetRandomPositionPoint());
+    }
+
+    private float GetRandomPositionPoint()
+    {
+        float randomPosition = Random.Range(minPosition, maxPosition);
+        float numSteps = Mathf.Floor(randomPosition / stepSize);
+        float adjustedPosition = numSteps * stepSize;
+
+        return adjustedPosition;
     }
 }
--- Assets/Scripts/Game/ScoreSystem.cs
+++ Assets/Scripts/Game/ScoreSystem.cs
@@ -5,16 +5,15 @@ using UnityEngine.UI;
 
 public class ScoreSystem : MonoBehaviour
 {
-    public static int currentScore = 100;
+    public static int currentScore = 0;
     public GameObject scoreValueText;
 
-    public void Start()
+    private void Update()
     {
         scoreValueText.GetComponent<Text>().text = currentScore.ToString();
     }
-    public void increaseScore()
+    public static void increaseScore()
     {
         currentScore += 1;
-        scoreValueText.GetComponent<Text>().text = currentScore.ToString();
     }
 }
